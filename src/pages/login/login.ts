0092169339836a3ec3232f7c601be617c42c07e6
Younhong <younhong@kakao.com>
2020-08-17 02:48:19 +0900

    reset password

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { SignupPage } from '../../pages/signup/signup';
 import * as firebase from 'firebase';
 import { LoaderProvider } from '../../providers/loader/loader';
@@ -24,6 +24,7 @@ export class LoginPage {
   }
 
   constructor(
+    private alertCtrl: AlertController,
     public navCtrl: NavController, 
     private loader: LoaderProvider,
     public navParams: NavParams) {
@@ -50,4 +51,44 @@ export class LoginPage {
     this.navCtrl.push(SignupPage);
   }
 
+  reset() {
+    let alert = this.alertCtrl.create({
+      title: "Reset Password",
+      message: "패스워드 재설정 링크를 받을 이메일 주소를 입력해주세요",
+      inputs: [
+        {
+          name: "email",
+          placeholder: "email"
+        }
+      ],
+      buttons: [
+        {
+          text: "취소",
+          role: "cancel",
+          handler: data => {
+            console.log("Cancel Clicked");
+          }
+        }, 
+        {
+          text: "확인",
+          handler: data => {     
+            var auth = firebase.auth();
+            var emailAddress = data.email;
+
+            auth.sendPasswordResetEmail(emailAddress).then(()=> {
+              let alert = this.alertCtrl.create({
+                title: "Password Reset Email",
+                subTitle: "사용자가 입력한 이메일로 패스워드 재설정 메일이 전송되었습니다",
+                buttons: ["확인"]
+              });
+              alert.present();
+            }).catch((error) => {
+
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
